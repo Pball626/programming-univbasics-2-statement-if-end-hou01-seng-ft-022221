c87152a43e41db7c3049d7cbe2655e791d870b31
Jose Mendoza <pball626@gmail.com>
2021-02-04 19:43:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,11 +4,11 @@ else
   puts "odd!"
 end
 
-current_time = Time.now
-current_time = current_time.to_i
+# #current_time = Time.now
+# #current_time = current_time.to_i
 
-if current_time.even?
-  puts "even!"
-else
-  puts "odd!"
-end
+# #if current_time.even?
+#   puts "even!"
+# else
+#   puts "odd!"
+# end
